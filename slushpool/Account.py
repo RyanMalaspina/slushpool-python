0a2bb67fcd89eb306e6e8adb65ab3ab263a1881e
Ryan Malaspina <ryan@malaspina.tech>
2018-12-21 20:37:14 -0500

    Initial Account request

--- slushpool/Account.py
+++ slushpool/Account.py
@@ -1,9 +1,14 @@
+import requests
+
+
 class Account:
     """ Class representation of a Slushpool Account """
 
     base_url = "https://slushpool.com/accounts/profile/json/"
 
-    def __init__(self):
+    def __init__(self, token):
+        self.url = Account.base_url + token
+
         self.username = ""
         self.rating = ""
         self.hashrate = ""
@@ -16,3 +21,27 @@ class Account:
         self.confirmed_nmc_reward = ""
         self.workers = {}
         self.wallet = ""
+
+        self.update()
+
+    def update(self):
+        """ Update the Account properties from Slushpool """
+        r = requests.get(self.url)
+
+        if r.status_code == 401:
+            raise RuntimeError("Invalid Slushpool token!")
+
+        r_json = r.json()
+
+        self.username = r_json['username']
+        self.rating = r_json['rating']
+        self.hashrate = r_json['hashrate']
+        self.send_threshold = r_json['send_threshold']
+        self.estimated_reward = r_json['estimated_reward']
+        self.confirmed_reward = r_json['confirmed_reward']
+        self.unconfirmed_reward = r_json['unconfirmed_reward']
+        self.nmc_send_threshold = r_json['nmc_send_threshold']
+        self.unconfirmed_nmc_reward = r_json['unconfirmed_nmc_reward']
+        self.confirmed_nmc_reward = r_json['confirmed_nmc_reward']
+        self.workers = r_json['workers']
+        self.wallet = r_json['wallet']
--- slushpool/__init__.py
+++ slushpool/__init__.py
@@ -5,5 +5,4 @@ from slushpool.Account import Account
 class Slushpool:
     """ Class Representation of Slushpool API Endpoints """
     def __init__(self, token):
-        self.stats = Stats(token)
         self.account = Account(token)
