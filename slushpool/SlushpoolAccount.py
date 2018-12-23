61699ae06ec3a0e6dbb70ebefb948728ec83d7dd
Ryan Malaspina <ryan@malaspina.tech>
2018-12-23 00:15:42 -0500

    Use Worker object in SlushpoolAccount

--- slushpool/SlushpoolAccount.py
+++ slushpool/SlushpoolAccount.py
@@ -43,15 +43,29 @@ class SlushpoolAccount:
         self.nmc_send_threshold = r_json['nmc_send_threshold']
         self.unconfirmed_nmc_reward = r_json['unconfirmed_nmc_reward']
         self.confirmed_nmc_reward = r_json['confirmed_nmc_reward']
-        self.workers = r_json['workers']
+        self.workers = self.__to_worker_objects(r_json['workers'])
         self.wallet = r_json['wallet']
+
+    def __to_worker_objects(self, workers_json_obj):
+        """ Converts the `workers` JSON object values to a dictionary with `Worker` object values.
+
+        This is so worker properties can be accessed like properties instead of dictionary attributes
+
+        `account.workers["name"].alive` instead of `account.workers["name"]["alive"]
+        """
+        workers = {}
+
+        for worker_name in workers_json_obj:
+            workers[worker_name] = Worker(workers_json_obj[worker_name])
+
+        return workers
 
 
 class Worker:
     """ Class Representation for an individual Worker """
-    def __init__(self):
-        self.last_share = 0
-        self.score = ""
-        self.alive = True
-        self.shares = 0
-        self.hashrate = 0
+    def __init__(self, worker_json_obj):
+        self.last_share = worker_json_obj['last_share']
+        self.score = worker_json_obj['score']
+        self.alive = worker_json_obj['alive']
+        self.shares = worker_json_obj['shares']
+        self.hashrate = worker_json_obj['hashrate']
