fce7ee3be15003a0debc5383541965c0d4aaf77c
Ryan Malaspina <ryan@malaspina.tech>
2018-12-23 13:47:53 -0500

    Bump to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slushpool",
-    version="1.0.0",
+    version="1.0.1",
     author="Ryan Malaspina",
     author_email="ryan@malaspina.tech",
     description="An API wrapper for Slushpool.com",
